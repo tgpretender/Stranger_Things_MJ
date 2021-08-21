73e406d9738d15e196b72c859ac2673e21039647
MESimo <84361264+MESimo@users.noreply.github.com>
2021-08-21 09:40:18 -0400

    temp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+//const baseUrl = 'https://strangers-things.herokuapp.com/api/2105-vpi-web-pt';
+
+
+
+
     return <div className="app"><h1>This is the react test</h1></div>
 }
 ReactDOM.render(<App />, document.getElementById('app'));
