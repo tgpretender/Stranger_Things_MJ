2314272d5a8c5782060bad84ab529ca126abf840
MESimo <84361264+MESimo@users.noreply.github.com>
2021-08-26 19:34:06 -0400

    got post data

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,2 +1,38 @@
 //This is for your initial fetch code once you perfect it on index.js
 //Remember to export it from here and then import it on both index.js and /components/index.js 
+import React, {useState} from 'react'
+
+const GetData = (props) => {
+    const {baseURL} = props;
+    //const [posts, setPosts] = useState('');
+
+    
+    fetch(`${baseURL}/posts`, {
+        method: "GET",
+        headers: {
+            'Content-Type': "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(res => console.log(res.data.posts))
+    
+    .catch(error => console.error(error));
+
+   
+    
+    return <div>GetData</div>
+}
+// const posts = res.data.posts;
+//        posts.forEach(post => {
+//            console.log(post);
+//            return post
+
+// useEffect(()=>{
+//     const  = <a GetData ></a>
+ 
+//     setUsername (posts)
+// }
+// ,[])
+
+
+export default GetData;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,25 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { useEffect } from 'react/cjs/react.production.min';
 
 import { 
     Header,
     Register,
     Login,
+    GetData,
     NewPost
 } from './components';
 
 const App = () => {
     const baseURL = 'https://strangers-things.herokuapp.com/api/2105-vpi-web-pt'
+
     const [ isAuthenticated, setIsAuthenticated ] = useState(false);
-    const [posts, setPosts] = useState([]);
     const [ userToken, setUserToken ] = useState("");
     const [ userName, setUserName ] = useState("Bob")
-
-   
-     async function getData () => {
-        const resp = await fetch('https://strangers-things.herokuapp.com/api/2105-vpi-web-pt');
-        const data = await resp.json();
-        console.log('data: ', data)
-        setPosts(data);
-    }
-   
-   
-       
-    
 
     return <div className="app">
         <Header isAuthenticated={isAuthenticated} userName={userName} />
         <main>
-            This is the main window where posts will appear
+           <GetData baseURL={baseURL} />
         </main>
         <section id="sidebar">
             <Register baseURL={baseURL} setUserToken={setUserToken} setUserName={setUserName} setIsAuthenticated={setIsAuthenticated} />
