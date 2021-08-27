1aa56a247762b9a63ff188b26e4c23071d51239c
tgpretender <jtpretender@yahoo.com>
2021-08-26 19:40:43 -0500

    creating branch

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,38 +1,46 @@
 //This is for your initial fetch code once you perfect it on index.js
 //Remember to export it from here and then import it on both index.js and /components/index.js 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const GetData = (props) => {
     const {baseURL} = props;
-    //const [posts, setPosts] = useState('');
 
-    
-    fetch(`${baseURL}/posts`, {
-        method: "GET",
-        headers: {
-            'Content-Type': "application/json"
+
+    async function getPosts() {
+        try {
+            const response = await fetch(`${baseURL}/posts`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': "application/json"
+                }
+            })
+            const data = await response.json();
+            return data;
         }
-    })
-    .then(res => res.json())
-    .then(res => console.log(res.data.posts))
-    
-    .catch(error => console.error(error));
+        catch (error) {
+            console.error(error);
+        }
+    }
+    const example = getPosts();
 
-   
-    
-    return <div>GetData</div>
+
+    // useEffect(()=>{
+//     const  = <a GetData ></a>
+ 
+//     setUsername (posts)
+// }
+// ,[])
+
+
+
+
+    return null
 }
 // const posts = res.data.posts;
 //        posts.forEach(post => {
 //            console.log(post);
 //            return post
 
-// useEffect(()=>{
-//     const  = <a GetData ></a>
- 
-//     setUsername (posts)
-// }
-// ,[])
 
 
 export default GetData;
