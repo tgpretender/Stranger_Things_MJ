131f95addaa98b7f062b3a30bc314bf9cedc191c
tgpretender <jtpretender@yahoo.com>
2021-08-28 18:42:44 -0500

    got profile working to display posts, will wait on Michael to test messages

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-const Profile = () => {
-    return <h1>Profile from component</h1>
+const Profile = (props) => {
+    const { baseURL, userToken, userName } = props;
+    const [ userMessages, setUserMessages ] = useState([])
+    const [ userPosts, setUserPosts ] = useState([])
+
+    useEffect(() => {
+        fetch(`${baseURL}/users/me`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userToken}`
+            }
+        })
+        .then(res => res.json())
+        .then((result) => {
+            const response = result.data;
+            setUserPosts(response.posts)
+            setUserMessages(response.messages)
+        })
+        .catch(err => console.error(err))
+    }, []);
+
+        userPosts.forEach(posts => {
+
+        });
+
+    return (<div className="profile">
+            <h1>{userName}'s Profile</h1>
+
+            <div className="profilePosts">
+                <h2>My Posts</h2>
+                {userPosts.map((post, index) => {
+                    const { _id: ID, author: {username}, createdAt, description, location, price, title, updatedAt, willDeliver} = post;
+                    const dayCreated = createdAt.slice(0,10);
+                    const timeCreated = createdAt.slice(11,16);
+                    const dayUpdated = updatedAt.slice(0,10);
+                    const timeUpdated = updatedAt.slice(11,16);
+            
+                    return (<div key={index} className="post">
+                                <div className="postHeading">
+                                    <div className="title">{title}</div>
+                                    <div className="author">{username}</div>
+                                </div>
+                                <div className="description">{description}</div>
+                                <div className="details">
+                                    <p><b>Location</b>: {location}</p>
+                                    <p><b>Price:</b> {price}</p>
+                                    <p><b>Will Deliver:</b> {willDeliver ? 'Yes' : 'No'}</p>
+                                    <p><b>Created:</b> {dayCreated}, {timeCreated}</p>
+                                    <p><b>Updated:</b> {dayUpdated}, {timeUpdated}</p>
+                                </div>
+                            </div>)
+                })}
+            </div>
+            <div className="profileMessages">
+                <h2>My Messages</h2>
+                { userMessages.map((message,index) => {
+                    <div className="message">Message</div>
+                })}
+            </div>
+        </div>)
 }
 
 export default Profile;
